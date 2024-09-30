9083387202e5feb3675bdccd7a421ae8ad1d1df4
lucasgrossi2 <lucasgrossi2005@gmail.com>
2024-09-30 19:39:50 -0300

    a

--- TP2/Q2.c
+++ TP2/Q2.c
@@ -6,23 +6,29 @@
 typedef struct{
     int id;
     int gen;
-    char name[80];
-    char desc[500];
-    char type1[20];
-    char type2[20];
-    char abls[10][80];
+    char name[160];
+    char desc[1000];
+    char type1[40];
+    char type2[40];
+    char ntypes[40];
+    char abls[200];
     double weight;
     double height;
     int capRate;
-    bool isLeg;
-
-}Pokemon;
-
-typedef struct{
+    char isLeg[40];
     int dia;
     int mes;
     int ano;
-}data;
+
+}Pokemon;
+
+bool lerEntrada(const char *entrada){
+    if(strcmp(entrada, "FIM") == 0){
+        return false;
+    } else {
+        return true;
+    }
+}
 
 int main(){
 
@@ -37,6 +43,14 @@ int main(){
 
        if (fgets(line, sizeof(line), file)){
 
+            strcpy(pokedex[i].ntypes, "TWO");
+
+            for(int j=0; j<strlen(line)-1; j++){
+                if (line[j] == ',' && line[j+1] == ','){
+                    strcpy(pokedex[i].ntypes, "ONE");
+                }
+            }
+
             char *token = strtok(line, ",");
             pokedex[i].id = atoi(token);
 
@@ -49,15 +63,24 @@ int main(){
             token = strtok(NULL, ",");
             strcpy(pokedex[i].desc, token);
 
+
+            //Tadpole Pokémon,water,,"['Swift Swim', 'Hydration', 'Water Absorb']"
+
+            //Proud Pokémon,normal,flying,"['Big Pecks', 'Super Luck', 'Rivalry']"
+
             token = strtok(NULL, ",");
             strcpy(pokedex[i].type1, token);
 
             token = strtok(NULL, ",");
-            strcpy(pokedex[i].type2, token); //existem casos em que essa string estará vazia e não será printada no final.
+            if(strcmp(pokedex[i].ntypes, "TWO")==0){
+                strcpy(pokedex[i].type2, token);
+            } else {
+                strcpy(pokedex[i].type2, "NULL");
+            }
 
-            //abls here
-
-            token = strtok(NULL,)
+            token = strtok(NULL, "\"");
+            //token = strtok(NULL, "\"");
+            strcpy(pokedex[i].abls, token);
 
             token = strtok(NULL, ",");
             pokedex[i].weight = atof(token);
@@ -69,10 +92,42 @@ int main(){
             pokedex[i].capRate = atoi(token);
 
             token = strtok(NULL, ",");
-            pokedex[i].isLeg = (strcmp(token, "true") == 0);
+            int leg = atoi(token);
+            if(leg == 0){
+                strcpy(pokedex[i].isLeg, "false");
+            } else {
+                strcpy(pokedex[i].isLeg, "true");
+            }
+
+
+            char *ultVirgula = strrchr(line, ',');
+            token = strtok(ultVirgula, "/");
+            pokedex[i].dia = atoi(token);
+            token = strtok(NULL, "/");
+            pokedex[i].mes = atoi(token);
+            token = strtok(NULL, "/");
+            pokedex[i].ano = atoi(token);
+
         }   
     }
 
+    char entrada[20];
+    int index;
+    int end = 0;
+
+    do{
+        scanf("%s", entrada);
+        index = atoi(entrada)-1;
+        if(lerEntrada(entrada)){
+            if (strcmp(pokedex[index].ntypes, "TWO")){ //somente 1 tipo
+            printf("[#%d -> %s: %s - ['%s'] - [%s - %.1lf - %.1lf - %d%% - %s - %d gen] - %d/%d/%d\n", pokedex[index].id, pokedex[index].name, pokedex[index].desc, pokedex[index].type1, pokedex[index].abls, pokedex[index].weight, pokedex[index].height, pokedex[index].capRate, pokedex[index].isLeg, pokedex[index].gen, pokedex[index].dia, pokedex[index].mes, pokedex[index].ano);
+            } else { //2 tipos
+                printf("[#%d -> %s: %s - ['%s', '%s'] - %s - %.1lf - %.1lf - %d%% - %s - %d gen] - %d/%d/%d\n", pokedex[index].id, pokedex[index].name, pokedex[index].desc, pokedex[index].type1, pokedex[index].type2, pokedex[index].abls, pokedex[index].weight, pokedex[index].height, pokedex[index].capRate, pokedex[index].isLeg, pokedex[index].gen, pokedex[index].dia, pokedex[index].mes, pokedex[index].ano);
+            }
+        } else {
+            end++;
+        }
+    }while(end == 0);
 
     fclose(file);
 
